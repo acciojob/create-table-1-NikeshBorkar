1d78e8a681d564b97d0e11c3ad030565f4625902
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-13 21:14:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.getElementById('sampleTable');
+	let btn = document.querySelector('input');
+	btn.addEventListener("click", addRow);
+	//let btn = document.querySelector('input');
+	//btn.addEventListener("click", addRow);
+
+	function addRow(){
+	//function addRow(){
+		let row = table.insertRow(0);
+		row.insertCell(0).innerHTML = "New Cell1";
+		row.insertCell(1).innerHTML = "New Cell2";
+	}
+ 
 }
